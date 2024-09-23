8cf768a2f446f009c5c4dd26874cc718e5da0962
H.D.Tài <tai17082006.tk@gmail.com>
2024-09-23 10:17:38 +0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,8 +9,10 @@ import shutil
 NODE_CLASS_MAPPINGS = {}
 NODE_DISPLAY_NAME_MAPPINGS = {}
 
+import comfy
+
 python = sys.executable
-extentions_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+extentions_folder = os.path.join(os.path.dirname(os.path.realpath(comfy.__file__)),
                                  "web" + os.sep + "extensions" + os.sep + "dzNodes")
 javascript_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)), "js")
 outdate_file_list = ['comfy_shared.js', 'debug.js', 'mtb_widgets.js', 'parse-css.js', 'dz_widgets.js']
